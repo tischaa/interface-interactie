d953f0505ce65f1ebf7cc5cd1067548609fc7eed
tischaa <146754073+tischaa@users.noreply.github.com>
2023-11-06 21:14:06 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,24 +4,30 @@ console.log("Hello Kitty!");
 // Tutorial: https://www.youtube.com/watch?v=TXcjupbEst4
 // Bron: https://stackoverflow.com/questions/15368733/how-to-toggle-two-images-onclick
 
-// Zoek de knop en de afbeeldingen
-const toggleButton = document.getElementById("toggle-button");
-const tvUitImage = document.getElementById("tv-uit-image");
-const tvAanImage = document.getElementById("tv-aan-image");
 
-// Voeg een eventlistener toe aan de knop om te reageren op klikgebeurtenissen
-toggleButton.addEventListener("click", function() {
-  // Controleer de huidige zichtbaarheid van de afbeeldingen
-  const tvUitVisible = window.getComputedStyle(tvUitImage).getPropertyValue("opacity") === "1";
+const tvButton = document.getElementById("toggle-button");
+const tvImage = document.getElementById("tv-uit-image");
+
+var tvAan = false;
+// const tvAanImage = document.getElementById("tv-aan-image");
+
+
+tvButton.addEventListener("click", function() {
   
-  // Wissel de zichtbaarheid van de afbeeldingen
-  if (tvUitVisible) {
-    tvUitImage.style.opacity = "0";
-    tvAanImage.style.opacity = "1";
-    toggleButton.querySelector("p").textContent = "TV Uit";
-  } else {
-    tvUitImage.style.opacity = "1";
-    tvAanImage.style.opacity = "0";
-    toggleButton.querySelector("p").textContent = "TV Aan";
+  // const tvUitVisible = window.getComputedStyle(tvUitImage).getPropertyValue("display") === "inline-block";
+
+  if (tvAan == true) {
+    // tv uitzetten
+    tvAan = false;
+    tvButton.innerHTML = "TV aan";
+    tvImage.src = "images/tv-uit.png";
   }
+  else {
+    // tv aanzet
+    tvAan = true;
+    tvButton.innerHTML = "TV uit";
+    tvImage.src = "images/tv-aan.png";
+  }
+
+
 });
