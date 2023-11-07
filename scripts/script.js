55da0ef72c353634e89286632fce54b991425275
tischaa <146754073+tischaa@users.noreply.github.com>
2023-11-07 09:44:50 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-// JavaScript Document
-console.log("Hello Kitty!");
+// // JavaScript Document
 
 // Tutorial: https://www.youtube.com/watch?v=TXcjupbEst4
 // Bron: https://stackoverflow.com/questions/15368733/how-to-toggle-two-images-onclick
@@ -23,11 +22,24 @@ tvButton.addEventListener("click", function() {
     tvImage.src = "images/tv-uit.png";
   }
   else {
-    // tv aanzet
+    // tv aanzetten
     tvAan = true;
     tvButton.innerHTML = "TV uit";
     tvImage.src = "images/tv-aan.png";
   }
-
-
 });
+
+const modal = document.querySelector("#modal1");
+const openModal = document.querySelector(".open-button1");
+const closeModal = document.querySelector("#close-button");
+
+openModal.addEventListener("click", () =>{
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () =>{
+  modal.close();
+});
+
+
+
